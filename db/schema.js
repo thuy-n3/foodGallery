0b96778d5ea019af3e69351efa5f61c6104b763e
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-19 16:23:36 -0500

    post schema built,some api routes

--- db/schema.js
+++ db/schema.js
@@ -10,13 +10,27 @@ const usersSchema = new Schema({
   email:     { type: String, required: true },
   password:  { type: String, required: true },
   // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x
-  
+
    // example of optional fields
   name:      { type: String },
   createdAt: { type: Date, default: Date.now }
 
 })
 
+//STEP ONE
+const dishSchema = new Schema ({ // establish the properties that we will use for the data
+    description: {type: String, required: true}, //some are required to fill out and others have defaults set
+    rating: {type: Number, required: true},
+    likes: {type: Number, default: 0},
+    location: {type: String, required: true},
+    title: {type: String, required: true},
+    authorEmail: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    tags: {type: [String], default: []},
+    authorId: {type: String, required: true}
+})
+
 module.exports = {
-  User: createModel('User', usersSchema)
+  User: createModel('User', usersSchema),
+  Dish: createModel('Dish', dishSchema) //STEP TWO (export model)
 }
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,12 +3,13 @@ const apiRouter = Router()
 let helpers = require('../config/helpers.js')
 
 let User = require('../db/schema.js').User
+let Dish = require('../db/schema.js').Dish //STEP THREE (import schema)
 
-  
+
   apiRouter
     .get('/users', function(req, res){
       User.find(req.query , "-password", function(err, results){
-        if(err) return res.json(err) 
+        if(err) return res.json(err)
         res.json(results)
       })
     })
@@ -16,7 +17,7 @@ let User = require('../db/schema.js').User
   apiRouter
     .get('/users/:_id', function(req, res){
       User.findById(req.params._id, "-password", function(err, record){
-        if(err || !record ) return res.json(err) 
+        if(err || !record ) return res.json(err)
         res.json(record)
       })
     })
@@ -25,7 +26,7 @@ let User = require('../db/schema.js').User
         if(err || !record) return res.json(err)
         let recordWithUpdates = helpers.updateFields(record, req.body)
         recordWithUpdates.save(function(err){
-          if(err) return res.json(err) 
+          if(err) return res.json(err)
           res.json(recordWithUpdates)
         })
       })
@@ -37,10 +38,48 @@ let User = require('../db/schema.js').User
           msg: `record ${req.params._id} successfully deleted`,
           _id: req.params._id
         })
-      })  
+      })
     })
 
     // Routes for a Model(resource) should have this structure
+
+//this route will create a brand new dish that we will put in the db
+apiRouter.post('/dishes', function(request, response) {
+    let dish = new Dish(request.body) //create new instance of schema from a MONGOOSE model, request.body is all the information that we have taken from the client side and we send it on the body of the request to the server
+    dish.save(function(error) { //saves to db
+        if(error) {
+            response.send(error)
+        }
+        else {
+            response.json(dish)
+        }
+    })
+})
+
+//this route will show us all the dishes posted by all users
+apiRouter.get('/dishes', function(request, response) {
+    Dish.find(request.query, function(error, records){  //some methods live directly on the model, so you don't need to create a new instance.
+    // request.query parses the parameters and turns them into an object (at this moment we have it just in case)
+        if(error) {
+            response.send(error)
+        }
+        else {
+            response.json(records)
+        }
+    })
+})
+
+//get dishes posted by the logged in user
+apiRouter.get('/user/dishes', function(request, response) {
+    Dish.find({authorId: request.user._id}, function(error, records) { //I want to get all dishes where the author id matches the current id of the user
+        if(error) {
+            response.send(error)
+        }
+        else {
+            response.json(records)
+        }
+    })
+})
 
 
 module.exports = apiRouter
