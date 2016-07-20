e4f2b7b47457b1af8ac8321f545f7df0df5df1f5
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-20 11:05:08 -0500

    adding some views

---
+++ src/scripts/views.js/header.js
@@ -0,0 +1,31 @@
+//COPIED FROM MONGO MESSAGES
+
+import React from 'react'
+import ACTIONS from '../actions'
+
+const Header = React.createClass({
+    render: function() {
+        return (
+            <div id="headerContainer">
+                <marquee height="50" behavior="alternate" direction="up" >Mongo Messages</marquee>
+                <NavBar />
+            </div>
+            )
+    }
+})
+
+const NavBar = React.createClass({
+    render: function() {
+        return (
+            <div id="navBar"> {/*made a few edits here so that is makes sense for this app*/}
+                <a href="#login">Log In</a>
+                <a href="#home">Home</a>
+                <a href="#dish/myDishes">My Dishes</a>
+                <a href="#dish/postDishes">Post Dish</a>
+                <a href="#" onClick={ACTIONS.logUserOut} >Log Out</a>
+            </div>
+            )
+    }
+})
+
+export default Header
