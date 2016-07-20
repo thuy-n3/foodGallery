a890b071e6c0bcf2e2ee1e84f2a0b35754378050
Caitlin Flattery <caitlinflattery@gmail.com>
2016-07-20 12:42:14 -0500

    added comments

--- src/scripts/actions.js
+++ src/scripts/actions.js
@@ -5,6 +5,7 @@ import {User} from './models/models'
 const ACTIONS = {
 
     //WE WANT TO LOG THE USER IN IMMEDIATELY AFTER THEY REGISTER (AS LONG AS THEY REGISTER SUCCESFULLY) THE FIRST METHOD REGISTERS AND THE SECOND LOGS THEM IN
+    //.then takes two callback functions, both of these methods use that to create either a 'success' function or a 'failure' function
     registerUser: function(userObj) { //input name doesn't actually matter, we just named it the same as the object that is getting passsed in for our own peace of mind
         User.register(userObj).then( () => ACTIONS.logUserIn(userObj.email, userObj.password),
             (error) => {
@@ -20,7 +21,7 @@ const ACTIONS = {
             (responseData) => {
                 alert(`user ${email} logged in!`)
                 console.log(responseData)
-                location.hash = 'home'
+                location.hash = 'home' //want the page to re-route to the home page after successfull login
             },
             (error) => {
                 alert('FAILURE LOGGING IN')
@@ -29,7 +30,7 @@ const ACTIONS = {
         )
     },
 
-    logUserOut: function() {
+    logUserOut: function() { // we want the page to reroute to the login page after a user has logged out (server keeps track os user being logged in with a 'session')
         User.logout().then(
             () => location.hash = 'login'
         )
